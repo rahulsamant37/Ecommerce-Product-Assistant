8ced8c9080022da9850806c7fcc626e70992dabb
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-08-31 16:24:58 +0530

    feat(ingestion): scaffold DataIngestion class for AstraDB pipeline
    - Set up method stubs for loading, transforming, and storing data
    - Prepared for CSV ingestion into AstraDB vector store
    - Load environment variables using dotenv

---
+++ Product_Assistant/etl/data_ingestion.py
@@ -0,0 +1,36 @@
+# from langchain_core.documents import Document
+# from langchain_astradb import AstraDBVectorStore
+# from Product_Assistant.utils.model_loader import ModelLoader
+# from Product_Assistant.utils.config_loader import load_config
+
+# import os
+# import pandas as pd
+# from typing import List
+from dotenv import load_dotenv
+load_dotenv()
+
+class DataIngestion:
+    """
+    Class to handle data transformation and ingestion into AstraDB vector store.
+    """
+    def __init__(self):
+        pass
+    
+    def _load_env_variables(self):
+        pass
+        
+    def _get_csv_path(self):
+        pass
+    
+    def _load_csv(self):
+        pass
+    
+    def transform_data(self):
+        pass
+    
+    def store_in_vector_db(self):
+        pass
+    
+    def run_pipeline(self):
+        pass
+    
